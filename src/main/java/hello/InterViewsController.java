bba06c1615cf895eff368404777446c677ebe194
MrRisoni <vanguelisk@gmail.com>
2020-07-05 18:41:59 +0300

    manually select fields

--- src/main/java/hello/InterViewsController.java
+++ src/main/java/hello/InterViewsController.java
@@ -1,16 +1,14 @@
 package hello;
 
 
-import models.InterViewSchedule;
-import models.InterViewStage;
-import models.JobOpening;
-import models.ReviewQuestionnaire;
+import models.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import repositories.InterViewsRepository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,7 +21,40 @@ public class InterViewsController {
     public HashMap<String,Object> getStages() {
         InterViewsRepository interRepo = new InterViewsRepository();
         HashMap<String ,Object> rsp = new HashMap<>();
-        rsp.put("stages",interRepo.fetchStagesAndApplicants(1L));
+        System.out.println("-------------------------");
+
+        List<InterViewStage> stages = interRepo.fetchStagesAndApplicants(1L);
+        List<InterViewStage> rspStages = new ArrayList<>();
+
+        for(InterViewStage stIterItm : stages) {
+            InterViewStage stg = new InterViewStage();
+            // ge the parts we need
+            stg.setId(stIterItm.getId());
+            stg.setTitle(stIterItm.getTitle());
+            List<InterViewGrade> grdlist = new ArrayList<>();
+            for (InterViewGrade grdItm : stIterItm.getGradesList()) {
+                InterViewGrade grd = new InterViewGrade();
+                grd.setId(grdItm.getId());
+                grd.setEvaluated(grdItm.getEvaluated());
+                grd.setEvaluated_at(grdItm.getEvaluated_at());
+                grd.setGrade(grdItm.getGrade());
+                grd.setNotes(grdItm.getNotes());
+                grd.setPassed(grdItm.getPassed());
+                JobApplication appleObj = new JobApplication();
+                appleObj.setDob(grdItm.getApplicationObj().getDob());
+                appleObj.setFull_name(grdItm.getApplicationObj().getFull_name());
+                appleObj.setId(grdItm.getApplicationObj().getId());
+
+                grd.setApplicationObj(appleObj);
+                grdlist.add(grd);
+            }
+            stg.setGradesList(grdlist);
+            rspStages.add(stg);
+        }
+
+        System.out.println("**************************");
+        rsp.put("stages",rspStages);
+
         rsp.put("opening",interRepo.fetchJobOpenings(1L));
         return rsp;
     }

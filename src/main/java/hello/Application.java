ee2e186747cc1aabe30231ffdaccf69de02d9723
MrRisoni <vanguelisk@gmail.com>
2020-07-10 10:51:09 +0300

    seems ok , skipped schema validation

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,9 @@ import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.HashMap;
@@ -19,6 +21,8 @@ import java.util.Map;
 @ComponentScan
 @EnableScheduling
 @SpringBootApplication
+@EntityScan("models")
+@EnableJpaRepositories("spring_repos")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/spring_repos/LanguageRepository.java
+++ src/main/java/spring_repos/LanguageRepository.java
@@ -2,8 +2,10 @@ package spring_repos;
 
 import models.Language;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LanguageRepository extends CrudRepository<Language,Long> {
 }

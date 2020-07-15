7d32f012a071667d1c7bb77a93e5f8d361ec281b
MrRisoni <vanguelisk@gmail.com>
2020-07-15 09:11:26 +0300

    spring security packages

---
+++ src/main/java/core/LoginController.java
@@ -0,0 +1,30 @@
+package core;
+
+import models.ReviewQuestionnaire;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/api/login", method = RequestMethod.GET)
+    public void login()
+    {
+
+    }
+
+    @RequestMapping(value = "/api/create_sesion", method = RequestMethod.GET)
+    public void create_sesion()
+    {
+
+    }
+
+    @RequestMapping(value = "/api/logout", method = RequestMethod.GET)
+    public void logout()
+    {
+
+    }
+}

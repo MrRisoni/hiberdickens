9e086da2d194135b9da55145571dbffe25851628
MrRisoni <vanguelisk@gmail.com>
2020-07-26 12:42:26 +0300

    days headers in timetabling

--- src/main/java/core/TimetableController.java
+++ src/main/java/core/TimetableController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import hqlmappers.TimetableDTO;
 
 import javax.persistence.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @Controller
@@ -29,20 +34,38 @@ public class TimetableController {
     {
         try {
             EntityManager entityManager= HibernateUtil.getEM();
-            System.out.println("###############################################");
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            DateFormat hourFormatter = new SimpleDateFormat("HH:mm");
 
-            TypedQuery<TimetableDTO> timetable = entityManager.createQuery("SELECT new hqlmappers.TimetableDTO(hs.id, gr.id,hr.id, crs.title,mb.name, ag.title, spd.title, hs.started, hs.duration, rm.title, hs.cancelled, hs.wage, hs.fee) " +
+            List<TimetableDTO> timetable = entityManager.createQuery("SELECT new hqlmappers.TimetableDTO(hs.id, gr.id,hr.id, crs.title,mb.name, ag.title, spd.title, hs.started, hs.duration, rm.title, hs.cancelled, hs.wage, hs.fee) " +
                     " FROM HistoryModel hs  JOIN hs.room rm " +
                     " JOIN hs.groupObj gr " +
                     " JOIN hs.hour hr " +
                     " JOIN gr.speedObj spd " +
                     " JOIN gr.ageObj ag " +
                     " JOIN hs.daskalos dsk JOIN dsk.member mb " +
-                    " JOIN gr.courseObj crs", TimetableDTO.class);
-            System.out.println("###############################################");
+                    " JOIN gr.courseObj crs" +
+                    " WHERE hs.started >= :starttime " +
+                    "  AND hs.started <= :endtime ORDER  BY hs.started ASC", TimetableDTO.class)
+                    .setParameter("starttime",WaterClock.getDate())
+                    .setParameter("endtime",WaterClock.getDateAWeekAhead())
+                    .getResultList();
 
-            modelo.addAttribute("timetable",timetable.getResultList());
+            List<String> days = new ArrayList<>();
 
+            days = timetable.stream().map(el -> {
+                try {
+                    return formatter.format(el.getStarted()).toString();
+
+                } catch (Exception ex) {
+                    return "";
+                }
+            }).collect(Collectors.toList());
+
+            days = days.stream().distinct().collect(Collectors.toList());
+            days.add("#");
+            days.add(0, "#");
+            modelo.addAttribute("days", days);
 
             return "timetable";
         }
